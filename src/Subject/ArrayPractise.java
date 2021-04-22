92cd501e5fef94b949f666eab6a37e3bb123c425
hamza <hamzacherfaoui32@yahoo.com>
2021-04-21 23:19:15 -0400

    An other one

---
+++ src/Subject/ArrayPractise.java
@@ -0,0 +1,12 @@
+package Subject;
+
+public class ArrayPractise {
+    public static void main(String[] args) {
+        String [] name={"John","Hamza"};
+//        System.out.println(name[0]);
+//        System.out.println(name[1]);
+        for(int i=0;i<2;i++){
+            System.out.println(name[i]);
+        }
+    }
+}
