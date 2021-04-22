93afb9369998a46a7e1a25058ba2c47460f17c3a
hamza <hamzacherfaoui32@yahoo.com>
2021-04-22 05:35:26 -0400

    Seventh commit

--- src/Subject/HashTableClass.java
+++ src/Subject/HashTableClass.java
@@ -1,8 +1,10 @@
+package Subject;
+
 import java.util.Hashtable;
 
-public class HashTabletry {
+public class HashTableClass {
     public static void main(String[] args) {
-        Hashtable <Integer,String> Student=new Hashtable<>();
+        Hashtable<Integer,String> Student=new Hashtable<>();
         Student.put(11,"Hamza");
         Student.put(12,"Steve");
         Student.put(13,"Mark");
